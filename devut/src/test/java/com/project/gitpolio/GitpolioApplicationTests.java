1a77875a706875f35b3227c983dbd0c9e3687e45
yusanggon <yusang5159@gmail.com>
2024-09-02 22:52:19 +0900

    project setting

---
+++ devut/src/test/java/com/project/gitpolio/GitpolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.gitpolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitpolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
